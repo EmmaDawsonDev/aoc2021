9ec281f3c79e8eded94a1ad9d6419ddfab9bc934
Emma Dawson <emma.l.dawson@gmail.com>
2021-12-08 20:26:42 +0100

    Complete day 6 part 2

--- day6.js
+++ day6.js
@@ -4,19 +4,53 @@ const data = `2,1,1,1,1,1,1,5,1,1,1,1,5,1,1,3,5,1,1,3,1,1,3,1,4,4,4,5,1,1,1,3,1,
 
 let dataArr = data.split(',')
 
-for (let i = 0; i < 80; i++) {
-  let newFish = []
-  for (let j = 0; j < dataArr.length; j++) {
-    if (+dataArr[j] > 0) {
-      dataArr[j]--
-    } else {
-      dataArr[j] = 6
-      newFish.push(8)
-    }
-    // console.log(newFish)
-  }
-  dataArr = dataArr.concat(newFish)
-  // console.log(`Day${i}`, dataArr)
+// for (let i = 0; i < 80; i++) {
+//   let newFish = []
+//   for (let j = 0; j < dataArr.length; j++) {
+//     if (+dataArr[j] > 0) {
+//       dataArr[j]--
+//     } else {
+//       dataArr[j] = 6
+//       newFish.push(8)
+//     }
+//     // console.log(newFish)
+//   }
+//   dataArr = dataArr.concat(newFish)
+//   // console.log(`Day${i}`, dataArr)
+// }
+
+// console.log(dataArr.length) // Part 1: 388739
+
+// set up data obj
+let dataObj = {
+  0: 0,
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+  7: 0,
+  8: 0,
 }
 
-console.log(dataArr.length) // Part 1: 388739
+// setup initial data
+for (let i = 0; i < dataArr.length; i++) {
+  dataObj[dataArr[i]]++
+}
+
+for (let j = 0; j < 256; j++) {
+  let temp = dataObj['0']
+  dataObj['0'] = dataObj['1']
+  dataObj['1'] = dataObj['2']
+  dataObj['2'] = dataObj['3']
+  dataObj['3'] = dataObj['4']
+  dataObj['4'] = dataObj['5']
+  dataObj['5'] = dataObj['6']
+  dataObj['6'] = dataObj['7'] + temp
+  dataObj['7'] = dataObj['8']
+  dataObj['8'] = temp
+}
+
+console.log(dataObj)
+console.log(Object.values(dataObj).reduce((a, b) => a + b, 0)) // Part 2: 1741362314973
